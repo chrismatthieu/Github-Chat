4a3948e87d8951870970da863e394094a8d307dd
Chris Matthieu <chris@matthieu.us>
2011-08-09 11:43:28 -0700

    added jquery on chat button

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,3 @@
-// require('/jquery-1.6.2.min.js');
-
-// $(document).ready(function(){
-
 var regex = /<span><span class=\"icon\"><\/span>New Issue<\/span><\/a>/g;
 var page;
 var hits;
@@ -12,9 +8,7 @@ var user;
 if (regex.test(document.documentElement.innerHTML)) {
   // The regular expression produced a match, so notify the background page.
 
-	// page = document.body.innerHTML;
 	page = document.documentElement.innerHTML;
-	
 	hits = page.match(regex);
 	
 	for (var i = 0; i < hits.length; i += 1) {
@@ -22,7 +16,7 @@ if (regex.test(document.documentElement.innerHTML)) {
 	// 	find the repo name i.e. GitHub.repoName = "nodester"
       var re1='(GitHub\\.repoName)';	// Fully Qualified Domain Name 1
       var re2='.*?';	// Non-greedy match on filler
-      var re3='((?:[a-z][a-z0-9_]*))';	// Variable Name 1
+      var re3='((?:[a-z][a-z0-9_-]*))';	// Variable Name 1
 
       var p = new RegExp(re1+re2+re3,["i"]);
       var m = p.exec(page);
@@ -31,16 +25,12 @@ if (regex.test(document.documentElement.innerHTML)) {
           var fqdn1=m[1];
           repo=m[2];
       }
+	};
 		
 	user = $('a.name').html();	
-	// alert(user);
-		
-	    page = page.replace(hits[i], '<span><span class="icon"></span>New Issue</span></a></li><li class><a href="#" onclick="javascript:window.open(\'http://groupim.heroku.com?chatroom=' + repo + '&username=' + user + '\', \'popup_id\', \'scrollbars,resizable,width=600,height=500\')"  class="minibutton btn-new-issue "><span><span class="icon"></span>Chat</span></a>');
-	// console.log('found: ' + trimString(hits[i]))
-	}
 	
-	// document.write (document.doctype + page);
-	document.write (page);
+	$('ul.actions').append('<li class><a href="#" onclick="javascript:window.open(\'http://groupim.heroku.com?chatroom=' + repo + '&username=' + user + '\', \'popup_id\', \'scrollbars,resizable,width=600,height=500\')"  class="minibutton btn-new-issue "><span><span class="icon"></span>Chat</span></a></li>');
+		
 	
   	chrome.extension.sendRequest({}, function(response) {});
 
@@ -49,10 +39,3 @@ if (regex.test(document.documentElement.innerHTML)) {
   // console.log('no matches')
 }
 
-// }
-
-//To trim leading or trailing spaces
-function trimString (str) {
-   // return str.replace(/^\s+/g, '').replace(/\s+$/g, '');  
-   return str.replace(/\s+/g, '').replace(/\(/g, '').replace(/\)/g, '').replace(/\./g, '').replace(/\-/g, '');  
-}
